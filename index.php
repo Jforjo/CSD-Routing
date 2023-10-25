ced804de8fe44905ebfc9c274a4402303430ec24
BuildTools <unconfigured@null.spigotmc.org>
2023-10-25 12:47:13 +0100

    Added a simple index page. Added a simple index page that has a link to the calculator page on, for now.

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CSD-Routing</title>
+</head>
+<body>
+    <h2>Temp index page</h2>
+    <ul>
+        <li>
+            <a href="/calculator.php">Click here to view the calculator</a>
+        </li>
+    </ul>
+</body>
+</html>

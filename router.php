efda622ff75793c2fbcfdc3ffa1c21b5fe2e5bd3
Jforjo <jaholmes2002@hotmail.com>
2023-10-30 12:21:05 +0000

    Created the files necessary for the router. Created the htaccess file that sends you to the router page that then routes the user depending on their URL. Also created some of the other relevent pages, including the 404 page.

---
+++ router.php
@@ -0,0 +1,18 @@
+<?php
+echo $_GET['url'];
+switch ($_GET['url']) {
+    case '/':
+        include_once(__DIR__ . '/index.php');
+        break;
+    case '/calculator':
+        include_once(__DIR__ . '/calculator.php');
+        break;
+    case '/currencyconverter':
+        include_once(__DIR__ . '/currencyconverter.php');
+        break;
+    default:
+        include_once(__DIR__ . '/include/404.php');
+        break;
+}
+
+?>

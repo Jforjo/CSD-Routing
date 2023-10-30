d904ff9ea374b58dee7783390aa392c901a78d8c
Jforjo <jaholmes2002@hotmail.com>
2023-10-30 12:40:56 +0000

    Added the link to the currency converter page onto the index page.

--- index.php
+++ index.php
@@ -9,7 +9,10 @@
     <h2>Temp index page</h2>
     <ul>
         <li>
-            <a href="/calculator.php">Click here to view the calculator</a>
+            <a href="/calculator">Click here to view the calculator</a>
+        </li>
+        <li>
+            <a href="/currencyconverter">Click here to view the currency converter</a>
         </li>
     </ul>
 </body>
